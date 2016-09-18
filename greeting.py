2b193c3f7de0ed0fdb3036e84e50f3ebd56ce794
rbb29 <rhvrabec@gmail.com>
2016-09-18 11:07:42 -0500

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,4 +1,4 @@
 #prompt user to enter name... and welcome them
 name = raw_input("Enter your name")
-print "Hello %s" % name
+print "Hello %s, we're really glad you've decided to own your data" % name
 
