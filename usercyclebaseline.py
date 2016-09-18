cfa51a930110bb02bab8b84fd0069d743cb65c44
rbb29 <rhvrabec@gmail.com>
2016-09-18 13:07:38 -0500

    Update usercyclebaseline.py

--- usercyclebaseline.py
+++ usercyclebaseline.py
@@ -1,7 +1,27 @@
 import datetime
 
 totaldays = raw_input("Enter the avg number of days you menstruate per month:")
+try:
+  totaldays = float(rawstr)
+except:
+  totaldays = 5
+
+if totaldays > 0:
+  print "Thanks!"
+else:
+  print "Please enter a number"
+  
 date = raw_input("Enter the day (1-30/31) of the month your period normally starts on:")
+try:
+  date = float(rawstr)
+except:
+  date = 1
+  
+if totaldays > 0:
+  print "Thanks!"
+else:
+  print "Please enter a number"
+  
 bl_endcycle = float(date) + float(totaldays)
 bl_startcycle = floate (date)
 
